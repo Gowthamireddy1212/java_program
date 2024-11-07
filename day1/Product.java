ae1585934d47b28ecee290cbfc4821f7a7d6fe32
Gowthamireddy1212 <gowthamireddyeruva@gmail.com>
2024-11-07 06:41:42 -0800

    day1

---
+++ day1/Product.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Product
+{
+public static void main(String[]args)
+{
+String Pname="laptop";
+String Pcolor="black";
+System.out.println("The Given Pname is:"+Pname);
+System.out.println("The Given Pcolor is:"+Pcolor);
+}
+}
